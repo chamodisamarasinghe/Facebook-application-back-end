c189fe73db39bb832ed3566db8404a25a1d9c642
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-08-26 20:29:31 +0530

    Updated file

--- routes/user.js
+++ routes/user.js
@@ -14,6 +14,20 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.json(user)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+
+
+
 router.post('/', async (req, res) => {
     const user = new User({
         firstName: req.body.firstName,
